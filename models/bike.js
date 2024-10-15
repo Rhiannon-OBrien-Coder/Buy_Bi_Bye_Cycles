04247969f0e76390a595755b789b6f68dffd2e58
siseldyke <siseldyke@gmail.com>
2024-10-15 13:40:00 -0400

    basic bike schema and database

---
+++ models/bike.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const  { Schema } = require('mongoose')
+
+const Bike = new Schema(
+    {
+        name: { type: String, required: true },
+        price: { type: Number, required: true },
+        type:{type: String, required: true},
+        brand:{type: String, required: true},
+        description: { type: String, required: true },
+        image: {type: String, required: true}
+    },
+    
+    { timestamps: true },
+)
+
+module.exports = mongoose.model('bikes', Bike)
