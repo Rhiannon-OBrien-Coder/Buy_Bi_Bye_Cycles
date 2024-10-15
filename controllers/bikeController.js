5e7fa2ce9a8d0de50bcdd45467d0b93eac069983
SpiritedVibes <austinrassman@gmail.com>
2024-10-15 14:08:58 -0400

    server.js and bikeController.js

---
+++ controllers/bikeController.js
@@ -0,0 +1,30 @@
+const Bike = require('../models/Bike')
+
+
+const getAllBikes = async (req, res) => {
+  try {
+    const bikes = await Bike.find({})
+    res.json(bikes)
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Error fetching bikes')
+  }
+}
+
+
+const getBikeById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const bike = await Bike.findById(id);
+    if (!bike) throw new Error('404 Bike not found');
+    res.json(bike);
+  } catch (error) {
+    console.error(error);
+    res.status(404).send('Bike not found');
+  }
+}
+
+module.exports = {
+  getAllBikes,
+  getBikeById
+}
