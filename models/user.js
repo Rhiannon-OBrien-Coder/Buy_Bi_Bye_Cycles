a6fcb5f5fbce972c828d6a03a24a8da6c0f737e1
siseldyke <siseldyke@gmail.com>
2024-10-15 14:04:09 -0400

    user model set up

---
+++ models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+const  { Schema } = require('mongoose')
+
+const User = new Schema(
+    {
+        name: { type: String, required: true },
+        adress: { type: String, required: true },
+        email: {type: String, required: true},
+        phoneNumber: { type: Number, required: true }
+    },
+    { timestamps: true },
+)
+
+module.exports = mongoose.model('users', User)
