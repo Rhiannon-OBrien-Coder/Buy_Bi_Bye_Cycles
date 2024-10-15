8d2962700caa3319ecb2b4843eb1cabecbb5e628
kpthanh01 <kphamthanh01@gmail.com>
2024-10-15 16:43:30 -0400

    fix localhoster listener and add navbar

--- server.js
+++ server.js
@@ -10,19 +10,17 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
-
 app.get('/', (req, res) => {
   res.send('This is root!')
 })
 
 app.get('/bikes', getAllBikes)
-app.get('/bikes/types/:type',getBikeByType)
-app.get('/bikes/brand/:Brand', getBikeByBrand)
+// app.get('/bikes/types/:type',getBikeByType)
+// app.get('/bikes/brand/:Brand', getBikeByBrand)
 app.get('/bikes/:id', getBikeById)
-app.post('/bikes', createBike)
-app.put('/bikes/:id', updateBike)
-app.delete('/bikes/:id', deleteBike)
+// app.post('/bikes', createBike)
+// app.put('/bikes/:id', updateBike)
+// app.delete('/bikes/:id', deleteBike)
 app.get('/users', getAllUsers)
 app.get('/users/:id', getUserById)
 
