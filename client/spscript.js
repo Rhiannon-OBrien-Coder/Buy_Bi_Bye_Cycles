ab36b842761c5c5aeed337b702d881d487298aea
rhiannon-obrien-coder <rhiannonobrien@gmail.com>
2024-10-16 15:02:42 -0500

    javascript now pulls from the database to populate images, prices, and names of bikes

--- client/spscript.js
+++ client/spscript.js
@@ -10,13 +10,33 @@ const liNine = document.querySelector('#btn9')
 
 async function images() {
     let response = await axios.get(
-        `https://localhost:3001/bikes`
+        `http://localhost:3001/bikes`
     )
-    let bikesArray = response
-    console.log (bikesArray)
-    // for (i=0; i<bikesArray.length; i++) {
-
-    // }
+    let bikesArray = response.data
+    for (i=0; i<bikesArray.length; i++) {
+        let image = bikesArray[i].image
+        let price = bikesArray[i].price
+        let name = bikesArray[i].name
+        if (i === 0) {
+            liOne.innerHTML= `<p>${name}</p><br><img src=${image} style="max-width: 150px; max-height: 100px;"><p>$${price}</p>`
+        } else if (i === 1) {
+            liTwo.innerHTML= `<p>${name}</p><br><img src=${image} style="max-width: 150px; max-height: 100px;"><p>$${price}</p>`
+        } else if (i === 2) {
+            liThree.innerHTML= `<p>${name}</p><br><img src=${image} style="max-width: 150px; max-height: 100px;"><p>$${price}</p>`
+        } else if (i === 3) {
+            liFour.innerHTML= `<p>${name}</p><br><img src=${image} style="max-width: 150px; max-height: 100px;"><p>$${price}</p>`
+        } else if (i === 4) {
+            liFive.innerHTML= `<p>${name}</p><br><img src=${image} style="max-width: 150px; max-height: 100px;"><p>$${price}</p>`
+        } else if (i === 5) {
+            liSix.innerHTML= `<p>${name}</p><br><img src=${image} style="max-width: 150px; max-height: 100px;"><p>$${price}</p>`
+        } else if (i === 6) {
+            liSeven.innerHTML= `<p>${name}</p><br><img src=${image} style="max-width: 150px; max-height: 100px;"><p>$${price}</p>`
+        } else if (i === 7) {
+            liEight.innerHTML= `<p>${name}</p><br><img src=${image} style="max-width: 150px; max-height: 100px;"><p>$${price}</p>`
+        } else if (i === 8) {
+            liNine.innerHTML= `<p>${name}</p><br><img src=${image} style="max-width: 150px; max-height: 100px;"><p>$${price}</p>`
+        }
+    }
 }
 
 images()
