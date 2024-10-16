a7f671273dcc30cddc388d3ad72973acd746ad50
SpiritedVibes <austinrassman@gmail.com>
2024-10-16 16:02:55 -0400

    script.js

--- client/script.js
+++ client/script.js
@@ -1,9 +1,12 @@
 const loginForm = document.getElementById('login-account')
 let userAccount = ``
+const hide = document.querySelector(`.login-btn`)
+const accountBtn = document.getElementById(`myAccount-btn`)
 loginForm.addEventListener('click', async (event) => {
   event.preventDefault()
   const email = document.getElementById('email-login').value
   const password = document.getElementById('password-login').value
+
 
   try {
     const response = await axios.get(`http://localhost:3001/users/${email}`)
@@ -12,8 +15,14 @@ loginForm.addEventListener('click', async (event) => {
     if (response.status === 200) {
       // Handle successful login
      // window.location.href = '/dashboard'
-     if (response.status === 200) {
-      const passwordResponse = await axios.get(`http://localhost:3001/users/${password-login}`) 
+     if (data.password===password) {
+      console.log(`correct password`)
+      hide.style.display = `none`
+      accountBtn.style.display = `block`
+
+
+      } else {
+        console.log(`wrong password`)
       }
     } else {
       document.getElementById('error').textContent = response.data.message
@@ -22,4 +31,5 @@ loginForm.addEventListener('click', async (event) => {
     console.error('Error:', error)
     document.getElementById('error').textContent = 'An error occurred. Please try again.'
   }
-})
+}) 
+
