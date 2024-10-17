7eb764cd4326dd84fdf31def0a6d11e6b7ce4e55
kpthanh01 <kphamthanh01@gmail.com>
2024-10-17 03:07:34 -0400

    optimize code & style; add user profile page; add update user features

--- client/script.js
+++ client/script.js
@@ -1,28 +1,113 @@
-const loginForm = document.getElementById('login-account')
+const loginBtn = document.getElementById('login-account')
+const loginContainer = document.querySelector('#login-form')
+const accountBtn = document.querySelector('#myAccount-btn')
+const loginMessage = document.querySelector('#login-message')
+const userProfile = document.querySelector('#user-profile')
+const welcomeMain = document.querySelector('main')
+
+const userName = document.querySelector('#nameText')
+const userAddress = document.querySelector('#addressText')
+const userEmail = document.querySelector('#emailText')
+const userPhone = document.querySelector('#phoneText')
+const profileTexts = document.querySelectorAll('.profile-text')
+
+const inputName = document.querySelector('#name')
+const inputAddress = document.querySelector('#address')
+const inputEmail = document.querySelector('#email')
+const inputPhone = document.querySelector('#phone')
+const profileInputs = document.querySelectorAll('.profile-input')
+
+const editBtn = document.querySelector('#profile-edit')
+const updateBtn = document.querySelector('#profile-save')
+
 let userAccount = ``
-const hide = document.querySelector(`.login-btn`)
-const accountBtn = document.getElementById(`myAccount-btn`)
-loginForm.addEventListener('click', async (event) => {
+
+const myAccount = () => {
+  welcomeMain.style.display = 'none'
+  userProfile.style.display = 'flex'
+
+  userName.innerHTML = userAccount.name
+  inputName.value = userAccount.name
+
+  userAddress.innerHTML = userAccount.address
+  inputAddress.value = userAccount.address
+
+  userEmail.innerHTML = userAccount.email
+  inputEmail.value = userAccount.email
+
+  userPhone.innerHTML = userAccount.phoneNumber
+  inputPhone.value = userAccount.phoneNumber
+}
+
+const editProfile = () => {
+  profileTexts.forEach(item => {
+    item.style.display = 'none'
+  })
+  profileInputs.forEach(item => {
+    item.style.display = 'block'
+  })
+
+  editBtn.style.display = 'none'
+  updateBtn.style.display = 'block'
+}
+
+const updateProfile = async () => {
+  let id = userAccount._id
+  let response = await axios({
+    method: 'put',
+    url: `http://localhost:3001/users/${id}`,
+    data: {
+      name: inputName.value,
+      email: inputEmail.value,
+      address: inputAddress.value,
+      phoneNumber: inputPhone.value,
+    }
+  })
+  if (response.status == 200) {
+    loginMessage.innerHTML = `Hello ${response.data.name}`
+    userName.innerHTML = response.data.name
+    inputName.value = response.data.name
+  
+    userAddress.innerHTML = response.data.address
+    inputAddress.value = response.data.address
+  
+    userEmail.innerHTML = response.data.email
+    inputEmail.value = response.data.email
+  
+    userPhone.innerHTML = response.data.phoneNumber
+    inputPhone.value = response.data.phoneNumber
+
+    profileTexts.forEach(item => {
+      item.style.display = 'block'
+    })
+    profileInputs.forEach(item => {
+      item.style.display = 'none'
+    })
+    editBtn.style.display = 'block'
+    updateBtn.style.display = 'none'
+    alert("Profile Updated")
+
+  } else {
+    alert("An error has occurred")
+  }
+}
+
+loginBtn.addEventListener('click', async (event) => {
   event.preventDefault()
   const email = document.getElementById('email-login').value
   const password = document.getElementById('password-login').value
-
-
   try {
     const response = await axios.get(`http://localhost:3001/users/${email}`)
     const data = response.data[0]
-  userAccount = data
     if (response.status === 200) {
-      // Handle successful login
-     // window.location.href = '/dashboard'
-     if (data.password===password) {
-      console.log(`correct password`)
-      hide.style.display = `none`
-      accountBtn.style.display = `block`
-
+      if (data.password === password) {
+        userAccount = data
+        loginMessage.innerHTML = `Hello ${data.name}`
+        loginContainer.style.display = 'none'
+        accountBtn.style.display = 'block'
 
       } else {
-        console.log(`wrong password`)
+        loginMessage.innerHTML = 'Cannot find User'
       }
     } else {
       document.getElementById('error').textContent = response.data.message
@@ -31,5 +116,5 @@ loginForm.addEventListener('click', async (event) => {
     console.error('Error:', error)
     document.getElementById('error').textContent = 'An error occurred. Please try again.'
   }
-}) 
+})
 
--- client/spscript.js
+++ client/spscript.js
@@ -1,41 +1,49 @@
-const liOne = document.querySelector('#btn1')
-const liTwo = document.querySelector('#btn2')
-const liThree = document.querySelector('#btn3')
-const liFour = document.querySelector('#btn4')
-const liFive = document.querySelector('#btn5')
-const liSix = document.querySelector('#btn6')
-const liSeven = document.querySelector('#btn7')
-const liEight = document.querySelector('#btn8')
-const liNine = document.querySelector('#btn9')
+// All these variables are not needed because of the listings.appendChild logic
+
+// const liOne = document.querySelector('#btn1')
+// const liTwo = document.querySelector('#btn2')
+// const liThree = document.querySelector('#btn3')
+// const liFour = document.querySelector('#btn4')
+// const liFive = document.querySelector('#btn5')
+// const liSix = document.querySelector('#btn6')
+// const liSeven = document.querySelector('#btn7')
+// const liEight = document.querySelector('#btn8')
+// const liNine = document.querySelector('#btn9')
+
+const listings = document.querySelector('.listings')
 
 async function images() {
-    let response = await axios.get(
-        `http://localhost:3001/bikes`
-    )
-    let bikesArray = response.data
-    for (i=0; i<bikesArray.length; i++) {
-        let image = bikesArray[i].image
-        let price = bikesArray[i].price
-        let name = bikesArray[i].name
-        if (i === 0) {
-            liOne.innerHTML= `<p>${name}</p><br><img src=${image} style="max-width: 150px; max-height: 100px;"><p>$${price}</p>`
-        } else if (i === 1) {
-            liTwo.innerHTML= `<p>${name}</p><br><img src=${image} style="max-width: 150px; max-height: 100px;"><p>$${price}</p>`
-        } else if (i === 2) {
-            liThree.innerHTML= `<p>${name}</p><br><img src=${image} style="max-width: 150px; max-height: 100px;"><p>$${price}</p>`
-        } else if (i === 3) {
-            liFour.innerHTML= `<p>${name}</p><br><img src=${image} style="max-width: 150px; max-height: 100px;"><p>$${price}</p>`
-        } else if (i === 4) {
-            liFive.innerHTML= `<p>${name}</p><br><img src=${image} style="max-width: 150px; max-height: 100px;"><p>$${price}</p>`
-        } else if (i === 5) {
-            liSix.innerHTML= `<p>${name}</p><br><img src=${image} style="max-width: 150px; max-height: 100px;"><p>$${price}</p>`
-        } else if (i === 6) {
-            liSeven.innerHTML= `<p>${name}</p><br><img src=${image} style="max-width: 150px; max-height: 100px;"><p>$${price}</p>`
-        } else if (i === 7) {
-            liEight.innerHTML= `<p>${name}</p><br><img src=${image} style="max-width: 150px; max-height: 100px;"><p>$${price}</p>`
-        } else if (i === 8) {
-            liNine.innerHTML= `<p>${name}</p><br><img src=${image} style="max-width: 150px; max-height: 100px;"><p>$${price}</p>`
-        }
+    let response = await axios.get(`http://localhost:3001/bikes`)
+    let bikeData = response.data
+
+    for (i=0; i<bikeData.length; i++) {
+        let image = bikeData[i].image
+        let price = bikeData[i].price
+        let name = bikeData[i].name
+
+        let bikeCard = document.createElement('button')
+        bikeCard.innerHTML = `<p>${name}</p><br><img src=${image}><p>$${price}</p>`
+        listings.appendChild(bikeCard)
+
+        // if (i === 0) {
+        //     liOne.innerHTML= `<p>${name}</p><br><img src=${image} style="max-width: 150px; max-height: 100px;"><p>$${price}</p>`
+        // } else if (i === 1) {
+        //     liTwo.innerHTML= `<p>${name}</p><br><img src=${image} style="max-width: 150px; max-height: 100px;"><p>$${price}</p>`
+        // } else if (i === 2) {
+        //     liThree.innerHTML= `<p>${name}</p><br><img src=${image} style="max-width: 150px; max-height: 100px;"><p>$${price}</p>`
+        // } else if (i === 3) {
+        //     liFour.innerHTML= `<p>${name}</p><br><img src=${image} style="max-width: 150px; max-height: 100px;"><p>$${price}</p>`
+        // } else if (i === 4) {
+        //     liFive.innerHTML= `<p>${name}</p><br><img src=${image} style="max-width: 150px; max-height: 100px;"><p>$${price}</p>`
+        // } else if (i === 5) {
+        //     liSix.innerHTML= `<p>${name}</p><br><img src=${image} style="max-width: 150px; max-height: 100px;"><p>$${price}</p>`
+        // } else if (i === 6) {
+        //     liSeven.innerHTML= `<p>${name}</p><br><img src=${image} style="max-width: 150px; max-height: 100px;"><p>$${price}</p>`
+        // } else if (i === 7) {
+        //     liEight.innerHTML= `<p>${name}</p><br><img src=${image} style="max-width: 150px; max-height: 100px;"><p>$${price}</p>`
+        // } else if (i === 8) {
+        //     liNine.innerHTML= `<p>${name}</p><br><img src=${image} style="max-width: 150px; max-height: 100px;"><p>$${price}</p>`
+        // }
     }
 }
 
